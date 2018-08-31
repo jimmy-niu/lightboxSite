2bf25b339fa6661f41bc1eba66455fdf687c5a8c
Jimmy Niu <chenenniu@gmail.com>
2018-08-31 04:38:53 -0400

    Added New Cards. Changed Font. Added buffer to lights

--- main.js
+++ main.js
@@ -1,47 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini|Mobile/i.test(navigator.userAgent)) {
         set_up_mobile();
-    } else{
+    } else {
         create_canvas();
     }
     bind_handlers();
 });
 
-$(window).on('resize', function(){
+$(window).on('resize', function () {
     resize_canvas();
 });
 
-function bind_handlers(){
-    $(document).on("mousemove", function(e){
+function bind_handlers() {
+    $(document).on("mousemove", function (e) {
         light_box(e.clientX, e.clientY);
     });
+
+
 }
 
-function set_up_mobile(){
+function set_up_mobile() {
     $("#cards").css("display", "none");
     $("#mobile_bar").css("display", "flex");
     create_canvas();
 }
 
-function checkHash(hash, obj){
+function checkHash(hash, obj) {
     return hash[obj] === true;
 }
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-  
+
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
-  
+
     return array;
-  }
+}
--- squares.js
+++ squares.js
@@ -1,78 +1,85 @@
 let lit_squares = [];
 let to_redraw = false;
 
+let old_x = 0;
+let old_y = 0;
+
 const SQUARE_SIZE = 15;
 const X_OFFSET = 10;
 const Y_OFFSET = 10;
 
-$(document).ready(function(){
+$(document).ready(function () {
     setInterval(fadeSquares, 500);
 });
 
 let possible_calls = [
-    function(index_x, index_y) {draw_from_index(index_x, index_y - 1)},
-    function(index_x, index_y) {draw_from_index(index_x, index_y + 1)},
-    function(index_x, index_y) {draw_from_index(index_x -1 , index_y)},
-    function(index_x, index_y) {draw_from_index(index_x + 1, index_y)}];
+    function (index_x, index_y) { draw_from_index(index_x, index_y - 1) },
+    function (index_x, index_y) { draw_from_index(index_x, index_y + 1) },
+    function (index_x, index_y) { draw_from_index(index_x - 1, index_y) },
+    function (index_x, index_y) { draw_from_index(index_x + 1, index_y) }];
 
-function light_box(x, y){
-    let index_x = Math.floor(x / (SQUARE_SIZE + X_OFFSET));
-    let index_y  = Math.floor(y / (SQUARE_SIZE + Y_OFFSET));
+function light_box(x, y) {
+    if (Math.abs(old_x - x) > 2 || Math.abs(old_y - y) > 2) {
+        let index_x = Math.floor(x / (SQUARE_SIZE + X_OFFSET));
+        let index_y = Math.floor(y / (SQUARE_SIZE + Y_OFFSET));
 
-    c.beginPath();
+        c.beginPath();
 
-    draw_from_index(index_x, index_y);
-    
-    let rand = Math.floor((Math.random() * 4) + 1);
-    possible_calls = shuffle(possible_calls);
-    
-    for(let i = 0; i < rand; i++){
-        possible_calls[i](index_x, index_y);
+        draw_from_index(index_x, index_y);
+
+        let rand = Math.floor((Math.random() * 4) + 1);
+        possible_calls = shuffle(possible_calls);
+
+        for (let i = 0; i < rand; i++) {
+            possible_calls[i](index_x, index_y);
+        }
+
+        c.closePath();
+        c.stroke();
+
+        setTimeout(fadeSquares, 1000);
+        to_redraw = true;
     }
-
-    c.closePath();
-    c.stroke();
-
-    setTimeout(fadeSquares, 1000);
-    to_redraw = true;
+    old_x = x;
+    old_y = y;
 }
 
-function draw_from_index(x, y){
+function draw_from_index(x, y) {
     c.fillStyle = "grey";
     c.globalAlpha = 1;
 
     let x_pixel = x * (X_OFFSET + SQUARE_SIZE) + X_OFFSET;
     let y_pixel = y * (Y_OFFSET + SQUARE_SIZE) + Y_OFFSET;
 
-    
+
     c.rect(x_pixel, y_pixel, SQUARE_SIZE, SQUARE_SIZE);
     c.fillRect(x_pixel, y_pixel, SQUARE_SIZE, SQUARE_SIZE);
 
-    let lit = {x: x_pixel, y: y_pixel, alpha: 0.1};
+    let lit = { x: x_pixel, y: y_pixel, alpha: 0.1 };
     lit_squares.push(lit);
 }
 
-function fadeSquares(){
+function fadeSquares() {
     let size = lit_squares.length;
-    for(let i = 0; i < size; i++){
+    for (let i = 0; i < size; i++) {
         let coor = lit_squares[i];
-        
+
         c.globalAlpha = coor.alpha;
-        c.lineWidth = '0.15';        
+        c.lineWidth = '0.15';
         c.fillStyle = "#050505";
-        c.strokeStyle = "ghostwhite";        
+        c.strokeStyle = "ghostwhite";
         c.fillRect(coor.x, coor.y, SQUARE_SIZE, SQUARE_SIZE);
-        
-        
+
+
         coor.alpha += 0.01;
 
-        if(coor.alpha >= 0.4){
+        if (coor.alpha >= 0.4) {
             lit_squares.splice(i, 1);
             size--;
         }
     }
 
-    if(to_redraw && lit_squares.length === 0){
+    if (to_redraw && lit_squares.length === 0) {
         resize_canvas();
         to_redraw = false;
     }
